f5a4ae23738f4ac3f5f6b43c620e296c7617187c
bhaktivaghasiya <125374026+bhaktivaghasiya@users.noreply.github.com>
2023-02-14 19:23:41 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,120 @@
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(5,0,-1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+# n=1
+# for i in range(6):
+#     for j in range(1,i+1):
+#         print(n,end=" ")
+#         n=n+1
+#     print()
+
+
+# for i in range(1,6):
+#     for j in range(1+i+1):
+#          print("*",end=" ")
+#     print()
+
+# n=1
+# for i in range(5):
+#     for j in range(5):
+#         print(n,end=" ")
+#     print()
+#     n=n+1
+
+# n=5
+# for i in range(5,0,-1):
+#     for j in range(5):
+#         print(n,end=" ")
+#     print()
+#     n=n-1
+
+# n=2
+# for i in range(5):
+#     for j in range(5):
+#         print(n,end=" ")
+#         print()
+#         n=n*2
+# n=1
+# for i in range(1,6):
+#     for j in range(1,n+1):
+#         print("*",end=" ")
+#     print()
+
+                                        # FOR LOOP
+
+
+# n=1
+# for i in range(5):                         
+#     for j in range(5):
+#         print(n,end=" ")
+#         print()
+#     n=n+1
+
+# n=5
+# for i in range(5,0,-1):
+#     for j in range(5,0):                   
+#         print(n,end=" ")
+#     print()
+#     n=5-1
+
+# n=2
+# for i in range(5):
+#     for j in range(5):                    
+#         print(n,end="  ")
+#     print()
+#     n=n+2
+
+# n=1
+# for i in range(5):
+#     for j in range(5):
+#         print(n,end=" ")                 
+#     print()
+#     n=n+2
+
+# n=1
+# for i in range(5):
+#     for j in range(5):                      
+#         print(n,end=" ")
+#         n=n+1
+#     print()
+
+
+
+# n=10
+# for i in range(5):
+#     for j in range(5):                      
+#         print(n,end=" ")
+#         n=n+11
+#     print()
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
